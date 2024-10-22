e5b3cf824ced7c7a7713c7e63c9dbcc0c40a29c0
aj3779672 <142121727+aj3779672@users.noreply.github.com>
2024-10-22 18:09:02 -0500

    Update TankDrive.java

--- TankDrive.java
+++ TankDrive.java
@@ -1,35 +1,36 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
 @TeleOp
-public class TankDrive extends OpMode{
-
-    private DcMotor leftMotor;
-    private DcMotor rightMotor;
-
+public class TankDrive extends OpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor armMotor;
     @Override
     public void init() {
-        leftMotor = hardwareMap.get(DcMotor.class, "left");
-        rightMotor = hardwareMap.get(DcMotor.class, "right");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        armMotor = hardwareMap.get(DcMotor.class, "arm");
 
-        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-
     @Override
     public void loop() {
         double leftPower = -gamepad1.left_stick_y;
         double rightPower = -gamepad1.right_stick_y;
-
+        boolean armFunction = gamepad1.y;
         leftPower = leftPower * leftPower * leftPower;
         rightPower = rightPower * rightPower * rightPower;
-
-        leftMotor.setPower(leftPower);
-        rightMotor.setPower(rightPower);
-
-
+        frontLeft.setPower(leftPower);
+        frontRight.setPower(rightPower);
+        backLeft.setPower(leftPower);
+        backRight.setPower(rightPower);
     }
 }
