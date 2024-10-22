16de0b9f7de42b20b531a9284fd84d750fa116be
actionrobtoix <actionrobotix@gmail.com>
2024-10-22 18:21:55 -0500

    Add files via upload

--- Autonomous.java
+++ Autonomous.java
@@ -1,41 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import static java.lang.Thread.sleep;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "name")
-public class Autonomous extends LinearOpMode{
+public class Autonomous extends LinearOpMode {
 
-    protected DcMotor frontLeft;
-    protected DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
 
     @Override
-    public void runOpMode() throws InterruptedException{
-        frontLeft = hardwareMap.get(DcMotor.class, "left");
-        frontRight = hardwareMap.get(DcMotor.class, "right");
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
-        moveForeward(1, 1000);
+        moveForward(1, 1000);
+        turnLeft(1,500);
+        moveForward(1, 1000);
+        turnLeft(1,500);
+        moveForward(1, 1000);
+        turnLeft(1,500);
+        moveForward(1, 1000);
+        turnLeft(1,500);
     }
-
-    public void waitForStart() {
-    }
-
-    public void moveForeward(int power, int time) throws InterruptedException {
+    public void moveForward (int power, int time){
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setPower(power);
         frontRight.setPower(power);
-
-        sleep(1000);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        sleep(time);
         frontLeft.setPower(0);
         frontRight.setPower(0);
-
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+    public void turnLeft (int power, int time){
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        sleep(time);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
     }
 }
