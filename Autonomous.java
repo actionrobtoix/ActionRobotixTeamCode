326ac2ced8c0785c6094c9e8d3c5db3d0a9f3f7a
actionrobtoix <actionrobotix@gmail.com>
2024-10-30 16:56:57 -0500

    Add files via upload

--- Autonomous.java
+++ Autonomous.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
-
+//ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "name")
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "Autonomous")
 public class Autonomous extends LinearOpMode {
 
     private DcMotor frontLeft;
@@ -24,16 +24,18 @@ public class Autonomous extends LinearOpMode {
 
         waitForStart();
 
-        moveForward(1, 1000);
-        turnLeft(1,500);
-        moveForward(1, 1000);
-        turnLeft(1,500);
-        moveForward(1, 1000);
-        turnLeft(1,500);
-        moveForward(1, 1000);
-        turnLeft(1,500);
+        moveForward((float) 0.5, 350);
+        turnRight((float) 0.5,340);
+        moveForward((float) 0.5, 300);
+        turnLeft((float) 0.5,340);
+        moveForward((float) 0.5, 300);
+        turnRight((float) 0.5,340);
+        moveForward((float) 0.5, 150);
+        turnRight((float) 0.5,340);
+        moveForward((float) 0.5, 550);
+
     }
-    public void moveForward (int power, int time){
+    public void moveForward (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setPower(power);
@@ -45,8 +47,22 @@ public class Autonomous extends LinearOpMode {
         frontRight.setPower(0);
         backLeft.setPower(0);
         backRight.setPower(0);
+        //ONE TILE DISTANCE IS 350 MILISECONDS AT 1 POWER
     }
-    public void turnLeft (int power, int time){
+    public void turnRight (float power, int time){
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        sleep(time);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+    public void turnLeft (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeft.setPower(power);
