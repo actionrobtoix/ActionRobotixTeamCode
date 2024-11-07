f6ce7ff6a608d8de772cbdfea4e73f19b1148522
actionrobtoix <actionrobotix@gmail.com>
2024-11-07 16:44:02 -0600

    Add files via upload

--- Autonomous.java
+++ Autonomous.java
@@ -24,16 +24,30 @@ public class Autonomous extends LinearOpMode {
 
         waitForStart();
 
-        moveForward((float) 0.5, 350);
-        turnRight((float) 0.5,340);
-        moveForward((float) 0.5, 300);
-        turnLeft((float) 0.5,340);
-        moveForward((float) 0.5, 300);
-        turnRight((float) 0.5,340);
-        moveForward((float) 0.5, 150);
-        turnRight((float) 0.5,340);
-        moveForward((float) 0.5, 550);
-
+        /*moveForward((float) 0.3, 1000);
+        sleep(250);*/
+        turnRight((float) 0.3,1475);
+        sleep(250);
+        turnLeft((float) 0.3,50);
+        moveForward((float) 0.3, 800);
+        /*sleep(1);
+        turnLeft((float) 0.5,680);
+        moveForward((float) 0.5, 600);
+        turnRight((float) 0.5,680);
+        moveForward((float) 0.5, 400);
+        turnRight((float) 0.5,680);
+        moveForward((float) 0.5, 1100);
+        moveBackward((float) 0.5, 1200);
+        turnLeft((float)0.5, 680);
+        turnRight((float)0.5, 680);
+        moveForward((float)0.5, 1200);
+        moveBackward((float)0.5, 1300);
+        turnLeft((float)0.5, 680);
+        turnRight((float)0.5, 680);
+        moveForward((float)0.5, 1300);
+        moveBackward((float)0.5, 1500);
+        turnRight((float) 0.5,680);
+        moveForward((float)0.5, 800);*/
     }
     public void moveForward (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -47,11 +61,29 @@ public class Autonomous extends LinearOpMode {
         frontRight.setPower(0);
         backLeft.setPower(0);
         backRight.setPower(0);
-        //ONE TILE DISTANCE IS 350 MILISECONDS AT 1 POWER
+        //ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
+    }
+    public void moveBackward (float power, int time){
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        sleep(time);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        //ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
     }
     public void turnRight (float power, int time){
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setPower(power);
         frontRight.setPower(power);
         backLeft.setPower(power);
@@ -65,6 +97,8 @@ public class Autonomous extends LinearOpMode {
     public void turnLeft (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeft.setPower(power);
         frontRight.setPower(power);
         backLeft.setPower(power);
