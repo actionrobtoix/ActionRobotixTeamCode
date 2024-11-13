ee53f2252214e252680390d662f9770666511618
actionrobtoix <actionrobotix@gmail.com>
2024-11-12 18:07:30 -0600

    Update TankDrive.java

--- TankDrive.java
+++ TankDrive.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class TankDrive extends OpMode {
@@ -14,9 +13,8 @@ public class TankDrive extends OpMode {
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor arm;
-    public Servo claw;
-    public Servo rotary;
-
+    public CRServo claw;
+    public CRServo rotary;
 
 
     @Override
@@ -26,8 +24,8 @@ public class TankDrive extends OpMode {
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         arm = hardwareMap.get(DcMotor.class, "arm");
-        claw = hardwareMap.get(Servo.class, "claw");
-        rotary = hardwareMap.get(Servo.class, "Rotary");
+        claw = hardwareMap.get(CRServo.class, "claw");
+        rotary = hardwareMap.get(CRServo.class, "Rotary");
 
         // Reverse the left motors
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -37,27 +35,30 @@ public class TankDrive extends OpMode {
     @Override
     public void loop() {
 
-        // claw control conditional
-        if (gamepad2.a) {
-            claw.setPosition(-1);
+        if(gamepad2.a){
+        claw.setPower(-1);
         }
-        if (gamepad2.b) {;
-        claw.setPosition(1);
-    }
-        if (gamepad2.y) {
-            rotary.setPosition(-1);
+        if(gamepad2.b){
+            claw.setPower(1);
         }
-        if (gamepad2.x) {;
-            rotary.setPosition(1);
+
+        if(gamepad2.y){
+            rotary.setPower(-1);
         }
+        if(gamepad2.x){
+            rotary.setPower(1);
+        }
+
+
+
+
 
         // Tank drive control
 
         double leftPower = -gamepad1.left_stick_y;
         double rightPower = -gamepad1.right_stick_y;
 
-        // claw control
-        boolean clawPower = gamepad2.a;
+       // claw control
 
         // Arm control
         float armPower = -gamepad2.right_stick_y;
@@ -76,7 +77,8 @@ public class TankDrive extends OpMode {
 
         // set actual value for arm and claw
         arm.setPower(armPower);
-        //claw.setPower(clawPower);
-    }
 
-}
+        }
+
+
+    }
