2b857e59f2acad5acc153ccbd4c242fca645d602
actionrobotix <actionrobotix@gmail.com>
2025-01-01 18:20:37 -0600

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -1,13 +1,13 @@
 package org.firstinspires.ftc.teamcode;
-//ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "Autonomous")
-public class Autonomous extends LinearOpMode {
-
+@TeleOp
+public class TankDrive extends OpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
@@ -17,192 +17,80 @@ public class Autonomous extends LinearOpMode {
     public Servo flip1;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         arm = hardwareMap.get(DcMotor.class, "arm");
-        claw = hardwareMap.get(Servo.class, "claw");
         flip1 = hardwareMap.get(Servo.class, "flip1");
+        claw = hardwareMap.get(Servo.class, "claw");
+        // Reverse the left motors
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    @Override
+    public void loop() {
+// claw control
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        if (gamepad1.a) {
+            claw.setPosition(1);
+        }
+        if (gamepad1.b) {
+            claw.setPosition(0.4);
+        }
+        if (gamepad2.a) {
+            //flip2.setPosition(1);
+            flip1.setPosition(0.7);
+        }
+        if (gamepad2.b) {
+            // flip2.setPosition(0);
+            flip1.setPosition(0);
+        }
+        /*if(gamepad2.y) {
+            flip1.setPosition(0.2);
+        }
+        if (gamepad2.x) {
+            flip1.setPosition(0.4);
+        }*/
 
-        waitForStart();
+        double forward = 1;
+        double back = 1;
+        if(gamepad1.left_bumper) {
+            backLeft.setPower(forward);
+            backRight.setPower(back);
+            frontLeft.setPower(back);
+            frontRight.setPower(forward);
+        }
+        else if(gamepad1.right_bumper)
+        {
 
-        moveForward((float)0.3, 800);
-        sleep(250);
-        moveArm(0.3, 1000);
-        sleep(250);
-        flipClaw(0.5);
-        sleep(250);
-        flipClaw(0);
-        sleep(250);
-        moveClaw(0);
-        sleep(250);
-        moveArm(0.3, 400);
-        sleep(250);
-        strafeLeft((float)0.3, 2000);
-        sleep(250);
-        moveForward((float)0.3, 800);
-        sleep(250);
-        strafeLeft((float)0.3, 300);
-        sleep(250);
-        moveBackward((float)0.3, 3000);
-        sleep(250);
-        moveForward((float)0.3, 4000);
-        sleep(250);
-        strafeRight((float)0.3, 500);
-        sleep(250);
-    }
-    public void moveForward (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-        //ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
-    }
-    public void moveBackward (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-        //ONE TILE DISTANCE IS 350 MILLISECONDS AT 1 POWER
-    }
-    public void turnRight (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void turnLeft (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void strafeLeft (float power, int time) {
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setPower(power);
-        frontRight.setPower(-power);
-        backLeft.setPower(-power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void strafeRight (float power, int time) {
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setPower(-power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(-power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void rightForward (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void rightBackward (float power, int time){
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void leftBackward (float power, int time){
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void leftForward (float power, int time){
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-        sleep(time);
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
-    }
-    public void moveClaw (double position){
-        claw.setPosition(position);
-    }
-    public void moveArm (double power, int time){
-        arm.setPower(power);
-        sleep(time);
-        arm.setPower(0);
-    }
-    public void flipClaw (double position){
-        flip1.setPosition(position);
+            backLeft.setPower(back);
+            backRight.setPower(forward);
+            frontLeft.setPower(forward);
+            frontRight.setPower(back);
+
+
+        }
+        // Tank drive control
+
+        double leftPower = -gamepad1.left_stick_y;
+        double rightPower = -gamepad1.right_stick_y;
+        // claw control
+
+        // Arm control
+        double armPower = (-gamepad2.right_stick_y)/2;
+
+        // Apply cubic scaling
+        leftPower = leftPower * leftPower * leftPower;
+        rightPower = rightPower * rightPower * rightPower;
+
+        // Set power for the drive motors
+        frontLeft.setPower(leftPower);
+        frontRight.setPower(rightPower);
+        backLeft.setPower(leftPower);
+        backRight.setPower(rightPower);
+        // set actual value for arm and claw
+        arm.setPower(armPower);
     }
 }
