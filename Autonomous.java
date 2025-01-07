7762909bc726d28334120765e3d4998ff318429a
actionrobotix <actionrobotix@gmail.com>
2025-01-07 12:00:11 -0600

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -15,7 +15,7 @@ public class Autonomous extends LinearOpMode {
     private DcMotor backRight;
     private DcMotor arm;
     public Servo claw;
-    public CRServo flip1;
+    public Servo flip1;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,26 +25,24 @@ public class Autonomous extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         arm = hardwareMap.get(DcMotor.class, "arm");
         claw = hardwareMap.get(Servo.class, "claw");
-        flip1 = hardwareMap.get(CRServo.class, "flip1");
+        flip1 = hardwareMap.get(Servo.class, "flip1");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 //-335
-        /*moveBackward((float)0.3, 915);
-        sleep(250);*/
-        flipClawBack(0.3, 20);
-        sleep(1250);
-        /*moveArm(0.3, 3000);
+        moveBackward((float)0.3, 915);
+        sleep(250);
+        flipClawBack(0.5);
+        sleep(250);
+        moveArm(0.7, 3000);
         sleep(250);
         moveForward((float)0.3, 200);
         sleep(250);
-        flipClaw(0.3, 150);
-        sleep(1250);
-        moveClaw(0);
+        flipClaw(0.5);
         sleep(250);
-        moveArm(0.3, 400);
+        moveClaw(0);
         sleep(250);
         moveArmBack(0.3, 4000);
         sleep(250);
@@ -65,7 +63,7 @@ public class Autonomous extends LinearOpMode {
         turnRight((float)0.3, 945);
         sleep(250);
         moveArm(0.3, 4000);
-        sleep(250);*/
+        sleep(250);
     }
     public void moveForward (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -219,11 +217,11 @@ public class Autonomous extends LinearOpMode {
         sleep(time);
         arm.setPower(0);
     }
-    public void flipClaw (double power, int time){
-        flip1.setPower(power);
+    public void flipClaw (double position){
+        flip1.setPosition(position);
     }
-    public void flipClawBack (double power, int time){
-        flip1.setDirection(DcMotorSimple.Direction.REVERSE);
-        flip1.setPower(power);
+    public void flipClawBack (double position){
+        flip1.setDirection(Servo.Direction.REVERSE);
+        flip1.setPosition(position);
     }
 }
