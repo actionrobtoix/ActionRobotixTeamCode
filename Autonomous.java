31faad0099af98c7bec1e9c28997845889999436
actionrobotix <actionrobotix@gmail.com>
2025-01-25 15:01:57 -0600

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -6,76 +6,95 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "Autonomous")
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous")
 public class Autonomous extends LinearOpMode {
 
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
-    private DcMotor arm;
-    public Servo claw;
-    public Servo flip1;
-
-    int position;
+    DcMotor frontLeft, frontRight, backLeft, backRight, horizontalSlide1, horizontalSlide2, verticalSlide1,verticalSlide2;
+    Servo  flip1, flip2, flipClaw, arm, claw;
+    CRServo intake1, intake2;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // Initialize hardware
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-        arm = hardwareMap.get(DcMotor.class, "arm");
+        arm = hardwareMap.get(Servo.class, "arm");
         claw = hardwareMap.get(Servo.class, "claw");
         flip1 = hardwareMap.get(Servo.class, "flip1");
-
-        // Set encoder for arm
-
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-//-170
-        /* strafeLeft( 0.3F, 500);
-         sleep(1000);
-         flipClawBack(0.3);
-         sleep(200);*/
+        
+        moveBackward(0.3F, 900);
 
-        moveBackward((float)0.3, 750);
-        sleep(200);
-        flipClaw(0.3);
-        sleep(250);
-        moveArm(1100);
-        sleep(500);
-        moveBackward((float)0.3,300);
-        sleep(250);
-        moveClaw(0);
-        sleep(250);
-        moveForward((float)0.3,250);
-        sleep(400);
-        flipClawBack(0.3);
-        sleep(250);
-        moveArm(0);
-        sleep(250);
-        strafeLeft(.5F,1400);
-        sleep(250);
-        moveBackward(0.5F, 1300);
-        sleep(250);
-        strafeLeft(0.5F,500);
-        sleep(250);
-        moveForward(0.5F,1600);
-        sleep(250);
-        turnLeft(0.3F, 80);
-        sleep(250);
-        moveBackward(0.5F, 1500);
-        sleep(250);
-        strafeLeft(0.5F, 675);
-        sleep(250);
-        moveForward(0.5F, 1550);
-        sleep(1000);
+       
+        followSpline(new double[]{0, 0}, new double[]{30, 60}, new double[]{60, 0}, 0.5F);
 
+       
     }
+
+    // Method to calculate spline points
+    public double[] calculateSplinePoint(double t, double[] P0, double[] P1, double[] P2) {
+        double x = Math.pow(1 - t, 2) * P0[0] + 2 * (1 - t) * t * P1[0] + Math.pow(t, 2) * P2[0];
+        double y = Math.pow(1 - t, 2) * P0[1] + 2 * (1 - t) * t * P1[1] + Math.pow(t, 2) * P2[1];
+        return new double[]{x, y};
+    }
+
+    // Method to follow a spline
+    public void followSpline(double[] P0, double[] P1, double[] P2, float power) {
+        double prevX = P0[0];
+        double prevY = P0[1];
+
+        for (double t = 0; t <= 1; t += 0.1) { // Increment t in small steps
+            double[] targetPoint = calculateSplinePoint(t, P0, P1, P2);
+            double targetX = targetPoint[0];
+            double targetY = targetPoint[1];
+
+            // Calculate distance and heading
+            double dx = targetX - prevX;
+            double dy = targetY - prevY;
+            double distance = Math.hypot(dx, dy);
+            double angle = Math.atan2(dy, dx);
+
+            // Move robot towards the next point
+            moveToTarget(power, angle, distance);
+
+            prevX = targetX;
+            prevY = targetY;
+        }
+    }
+
+    // Method to move to a target point
+    public void moveToTarget(float power, double angle, double distance) {
+        double xPower = power * Math.cos(angle);
+        double yPower = power * Math.sin(angle);
+
+        frontLeft.setPower(yPower + xPower);
+        frontRight.setPower(yPower - xPower);
+        backLeft.setPower(yPower + xPower);
+        backRight.setPower(yPower - xPower);
+
+        sleep((int) (distance * 350)); // Adjust based on tile distance
+        stopMotors();
+    }
+
+    // Stop all motors
+    public void stopMotors() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+
+
+
+
 
     public void moveForward (float power, int time){
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -218,36 +237,46 @@ public class Autonomous extends LinearOpMode {
     public void moveClaw (double position){
         claw.setPosition(position);
     }
-    public void moveArm (double power, int time){
-        arm.setPower(power);
+
+    public void collectIntake (int time){
+        intake1.setPower(-1);
+        intake2.setPower(1);
         sleep(time);
-        arm.setPower(0);
+        intake1.setPower(0);
+        intake2.setPower(0);
+
     }
-    public void moveArmBack (double power, int time){
-        arm.setDirection(DcMotorSimple.Direction.REVERSE);
-        arm.setPower(power);
+    public void releaseIntake (int time){
+        intake1.setPower(1);
+        intake2.setPower(-1);
         sleep(time);
-        arm.setPower(0);
+        intake1.setPower(0);
+        intake2.setPower(0);
+
     }
-    public void flipClaw (double position){
+    public void moveVSlide (double power, int time){
+        verticalSlide1.setPower(power);
+        verticalSlide2.setPower(power);
+        sleep(time);
+        verticalSlide1.setPower(0);
+        verticalSlide2.setPower(0);
+    }
+
+    public void moveHorizontalSlide (double power, int time){
+        horizontalSlide1.setPower(power);
+        horizontalSlide2.setPower(power);
+        sleep(time);
+        horizontalSlide1.setPower(0);
+        horizontalSlide2.setPower(0);
+    }
+   
+    public void flipArm (double position){
         flip1.setPosition(position);
+        flip2.setPosition(position);
+        
     }
-    public void flipClawBack (double position){
-        flip1.setDirection(Servo.Direction.REVERSE);
-        flip1.setPosition(position);
-    }
+    
 
-    public void moveArm(int ticks) {
-        arm.setTargetPosition(ticks); // Define target position
-        arm.setPower(0.3); // Reduced power for slower movement
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION); // Move to position using encoders
-
-        while (arm.isBusy()) {
-            telemetry.addData("Arm Position", arm.getCurrentPosition());
-            telemetry.update();
-        }
-
-        arm.setPower(0); // Stop motor once target is reached
-    }
+    
 
 }
